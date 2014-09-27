0383cbdf969b1cf2e703906d09a56d4c4a0819d4
Adrian Carpenter <adriatic.c@gmail.com>
2014-09-27 00:26:52 -0700

    Perfection paranoia (SQL databases drive me crazy)

--- migrations/0001_user_id.test.sh
+++ migrations/0001_user_id.test.sh
@@ -3,8 +3,8 @@ main () {
 #############################################################################
 
 compare Ideas\
-    'SELECT user.name,idea.title FROM user LEFT JOIN idea ON idea.user_id=user.id;'\
-    'SELECT user.name,idea.title FROM user LEFT JOIN idea ON idea.user_id=user.id;'
+    'SELECT user.name,idea.title,idea.short_write_up FROM user LEFT JOIN idea ON idea.user_id=user.id;'\
+    'SELECT user.name,idea.title,idea.short_write_up FROM user LEFT JOIN idea ON idea.user_id=user.id;'
 
 compare Improvements\
     'SELECT user.name,improvement.content FROM user LEFT JOIN improvement ON improvement.user_id=user.id;'\
@@ -23,11 +23,16 @@ compare () {
     echo "$3"|sqlite3 after.db >after
     diff -u before after >/dev/null && echo -e "\033[32m$1 OK\033[0m" || echo -e "\033[31m$1 FAILED\033[0m"
 }
+cd $(dirname $(readlink -f $0))
 BASE=$(basename -s .test.sh $0)
 echo "Testing ${BASE}..."
 BEFORE=${BASE}.before.sql
 AFTER=${BASE}.after.sql
 sqlite3 before.db ".read $BEFORE"
 sqlite3 after.db ".read $AFTER"
+sqlite3 before.db ".schema" >before
+sqlite3 after.db ".schema" >after
+echo -e "\033[33mSchema diff\033[0m" 
+diff -u before after
 main
 rm before.db after.db before after

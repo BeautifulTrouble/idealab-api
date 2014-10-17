c32e1019951d3dff0031443aacbb27a67b060838
Adrian Carpenter <adriatic.c@gmail.com>
2014-10-17 10:44:45 -0700

    Update import script for new spreadsheet format

--- import.py
+++ import.py
@@ -3,7 +3,6 @@
 import contextlib
 import csv
 import os, os.path
-import platform
 import shutil
 import time
 import urllib2
@@ -13,15 +12,10 @@ except ImportError:
     from pysqlite2 import dbapi2 as sqlite3
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
-if platform.node() == 'change':
-    DB_FILE = 'idealab.db'
-    DB_PATH = THIS_DIR + '/' + DB_FILE
-    DOC_ID = '1EJRqTOjDbwV6iNf9tfL2Rz7g8OrDaiYvxLFc1ar1598'
-    CSV_URL = 'https://docs.google.com/spreadsheets/d/{}/export?format=csv'.format(DOC_ID)
-else:
-    DB_FILE = 'dummy.db'
-    DB_PATH = THIS_DIR + '/' + DB_FILE
-    CSV_URL = 'http://localhost:8000/dummy.csv'
+DB_FILE = 'idealab.db'
+DB_PATH = THIS_DIR + '/' + DB_FILE
+DOC_ID = '1EJRqTOjDbwV6iNf9tfL2Rz7g8OrDaiYvxLFc1ar1598'
+CSV_URL = 'https://docs.google.com/spreadsheets/d/{}/export?format=csv'.format(DOC_ID)
 
 def backup_db(path=THIS_DIR + '/backups'):
     try:
@@ -39,7 +33,7 @@ def main():
     db = sqlite3.connect(DB_PATH)
     c = db.cursor()
     with contextlib.closing(urllib2.urlopen(CSV_URL)) as file:
-        for id,published,title,short_write_up in utf8izer(csv.reader(file)):
+        for id,published,name,contact,title,short_write_up in utf8izer(csv.reader(file)):
             published = '1' if published == '1' else '0'
             c.execute('''
                 UPDATE idea 
